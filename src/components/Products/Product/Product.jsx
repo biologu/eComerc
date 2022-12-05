475740ed11241ec4232f938b0d4e9e32ee957bcd
marjen2022 <marjenmurati@gmail.com>
2022-12-05 13:40:23 +0100

    shtova pak stilizim te produkteve dhe

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import {Card, CardMedia, CardContent, CardActions, Typography, IconBotton } from '@material-ui/core';
+import { AddShoppingCart } from '@muaterial-ui/icons';
+import useStyles from './styles';
 
-const Product = () => {
+// import { ClassNames } from '@emotion/react';
+// import { Label } from '@mui/icons-material';
+const Product = ({ product }) => {
+  const classes = useStyles();
+
   return (
-    <div>
-
-    </div>
+    <Card className={classes.root}>
+      <CardMedia className={classes.media} image='' title={product.name}  />
+<CardContent>
+    <div className={classes.cardContent}>
+      <Typography variant="h5" gutterBottom>
+         {product.name}
+      </Typography>
+      <Typography variant="h5">
+         {product.price}
+      </Typography>
+      </div>
+      <Typography variant="h2" color="textSecondary">{product.description}</Typography>
+</CardContent>
+<CardActions disableSpacing className={classes.CardActions}>
+    <IconBotton aria-label="Add to Cart">
+      <AddShoppingCart/>
+     </IconBotton>
+</CardActions>
+    </Card>
   )
 }
 
